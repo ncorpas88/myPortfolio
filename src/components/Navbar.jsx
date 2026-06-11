3cfbdd0ee9020aae8eb39f9dc821d26d38e06417
Natanael Corpas Rivero <natel1988@gmail.com>
2026-06-11 11:56:54 +0200

    actualizaciones2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,69 @@
-import { Link } from "react-router-dom";
-import { Home, FolderKanban, Mail } from "lucide-react";
+import { Link, useLocation } from "react-router-dom";
+import { Home, FolderKanban, Mail, Menu, X } from "lucide-react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { useState } from "react";
 import "../styles/navbar.css";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+  const location = useLocation();
+
+  const close = () => setOpen(false);
+
   return (
     <nav className="navbar">
-      <div className="avatarcountainer">
-        <div className="avatar">
-          <img src="images/avatar.png" alt="avatar" className="avatar-img" />
-        </div>
+      <div className="avatar">
+        <img src="images/avatar.png" alt="avatar" />
       </div>
-      <div className="navbar-links">
-        <Link to="/" className="nav-button">
-          <Home size={18} />
-          Home
+
+      <button
+        className="hamburger"
+        onClick={() => setOpen((o) => !o)}
+        aria-label="Toggle menu"
+      >
+        {open ? <X size={24} /> : <Menu size={24} />}
+      </button>
+
+      <div className={`navbar-links ${open ? "open" : ""}`}>
+        <Link
+          to="/"
+          className={`nav-button ${location.pathname === "/" ? "active" : ""}`}
+          onClick={close}
+        >
+          <Home size={16} /> Home
         </Link>
-        <Link to="/projects" className="nav-button">
-          <FolderKanban size={18} />
-          Projects
+
+        <Link
+          to="/projects"
+          className={`nav-button ${location.pathname === "/projects" ? "active" : ""}`}
+          onClick={close}
+        >
+          <FolderKanban size={16} /> Projects
         </Link>
-        <Link to="https://github.com/ncorpas88" className="nav-button">
-          <FaGithub size={18} />
-          GitHub
-        </Link>
-        <a href="mailto:natel1988@gmail.com" className="nav-button">
-          <Mail size={18} />
-          Mail
+
+        <a
+          href="https://github.com/ncorpas88"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="nav-button"
+          onClick={close}
+        >
+          <FaGithub size={16} /> GitHub
         </a>
-        <Link to="https://www.linkedin.com/in/ncorpas/" className="nav-button">
-          <FaLinkedin size={18} />
-          Linkedin
-        </Link>
+
+        <a href="mailto:natel1988@gmail.com" className="nav-button" onClick={close}>
+          <Mail size={16} /> Mail
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/ncorpas/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="nav-button"
+          onClick={close}
+        >
+          <FaLinkedin size={16} /> LinkedIn
+        </a>
       </div>
     </nav>
   );
